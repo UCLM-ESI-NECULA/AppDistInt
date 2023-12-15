26259ed1a191c08582b3a9b5837a369670b2fe67
vale <valentin.necula02@gmail.com>
2023-12-15 22:31:21 +0100

    feat: changed run script

--- run.sh
+++ run.sh
@@ -6,18 +6,21 @@ if [ "$(docker ps -q -f name=blob-api-container)" ]; then
     exit 1
 fi
 
-# Set the default storage folder if not provided in the environment variable
-BLOB_STORAGE_FOLDER=${BLOB_STORAGE_FOLDER:-"$PWD/storage"}
+# Use provided port or default to 3002
+BLOB_SERVICE_PORT=${1:-3002}
+
+# Use provided storage folder or default to "$PWD/storage"
+BLOB_STORAGE_FOLDER="${PWD}/${2:-storage}"
 
 # Check if the storage folder exists, create if it does not
 if [ ! -d "$BLOB_STORAGE_FOLDER" ]; then
-    echo "Storage folder not found. Creating: $BLOB_STORAGE_FOLDER"
-    mkdir -p "$BLOB_STORAGE_FOLDER"
+    echo "Storage folder not found. Creating: ${PWD}/$BLOB_STORAGE_FOLDER"
+    mkdir -p "${PWD}/$BLOB_STORAGE_FOLDER"
 fi
 
 # Run the Docker container with volume mapping
 docker run --rm --name blob-api-container \
-  -p "${BLOB_SERVICE_PORT:-3002}:3002" \
+  -p "${BLOB_SERVICE_PORT}:3002" \
   -v "$BLOB_STORAGE_FOLDER":/usr/src/app/storage \
   --cpus="1.0" --memory="2g" \
   blob-api-image
